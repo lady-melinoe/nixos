bef3f66afb09295d7e24165f660bd7811996b56a
Melinoe <admin@melinoe.net>
2025-11-21 21:48:02 +0800

    fix array mount to use fsType/mountpoint instead of content block

--- nodes/benzaiten/disk-config.nix
+++ nodes/benzaiten/disk-config.nix
@@ -60,10 +60,8 @@
     nodev = {
       "array-1" = {
         device = "/dev/disk/by-label/array-1";
-        content = {
-          type = "btrfs";
-          mountpoint = "/array";
-        };
+        fsType = "btrfs";
+        mountpoint = "/array";
       };
     };
   };
