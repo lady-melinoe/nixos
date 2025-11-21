e7eb8054e985f546c519c80f3fe1aeb9c206280e
Melinoe <admin@melinoe.net>
2025-11-21 21:55:07 +0800

    add jq to system packages

--- common/settings.nix
+++ common/settings.nix
@@ -7,7 +7,7 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.require-sigs = false;
   system.stateVersion = "25.05";
-  environment.systemPackages = [ pkgs.git pkgs.tcpdump pkgs.nftables ];
+  environment.systemPackages = [ pkgs.git pkgs.tcpdump pkgs.nftables pkgs.jq ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernel.sysctl = {
     "net.ipv6.conf.all.autoconf" = false;
