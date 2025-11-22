57e36986aafe70b3b28f59955a7f354c76fa447b
Melinoe <admin@melinoe.net>
2025-11-22 10:03:32 +0800

    minecraft is tcp only, drop udp rule

--- common/firewall.nix
+++ common/firewall.nix
@@ -59,7 +59,6 @@
         iifname $inet_ifs tcp dport { 80, 443 } dnat to 198.18.1.1
         iifname $inet_ifs tcp dport { 993, 25, 465 } dnat to 198.18.1.6
         iifname $inet_ifs tcp dport { 25565 } dnat to 198.18.1.8
-        iifname $inet_ifs udp dport { 25565 } dnat to 198.18.1.8
       }
 
       chain postrouting {
