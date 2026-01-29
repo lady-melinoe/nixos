2519e73e18bc7b98ca75f25f159dba2f32ede4e7
Melinoe <admin@melinoe.net>
2026-01-30 05:19:40 +0800

    fix wireguard config

--- common/wireguard.nix
+++ common/wireguard.nix
@@ -4,14 +4,11 @@ let
   cfg = config.melinoe;
   keys = import ./wg-keys.nix;
   basePort = 64512;
-  hostId = cfg.nodeId;
-  localListenPort = basePort + hostId;
   wgPrefix = "198.19.3";
-  localWgAddr = "${wgPrefix}.${toString hostId}";
+  localWgAddr = "${wgPrefix}.${toString cfg.nodeId}";
 
   peerToCfg = peer: let
     pid = peer.id;
-    port = basePort + pid;
     allowed = lib.unique (peer.allowedIPs ++ [ "${wgPrefix}.0/24" "${wgPrefix}.${toString pid}/32" ]);
   in {
     publicKey = keys."${toString pid}";
@@ -20,17 +17,25 @@ let
     persistentKeepalive = peer.persistentKeepalive;
   };
   extraBgpPeers = map (p: { id = p.id; addr = "${wgPrefix}.${toString p.id}"; }) cfg.wgPeers;
+
+  mkInterface = peer: {
+    name = "wg-${toString peer.id}";
+    value = {
+      ips = [ "${localWgAddr}/32" ];
+      listenPort = basePort + peer.id;
+      privateKeyFile = "/etc/melinoe/wg-${config.networking.hostName}.key";
+      peers = [ (peerToCfg peer) ];
+    };
+  };
+
+  interfaces = lib.listToAttrs (map mkInterface cfg.wgPeers);
+  ports = map (peer: basePort + peer.id) cfg.wgPeers;
 in {
   config = lib.mkIf (cfg.wgPeers != [ ]) {
-    networking.wireguard.interfaces."wg-mesh" = {
-      ips = [ "${localWgAddr}/32" ];
-      listenPort = localListenPort;
-      privateKeyFile = "/etc/melinoe/wg-${config.networking.hostName}.key";
-      peers = map peerToCfg cfg.wgPeers;
-    };
+    networking.wireguard.interfaces = interfaces;
 
     # Open just the ports we actually listen on.
-    melinoe.wgPorts = [ localListenPort ];
+    melinoe.wgPorts = ports;
 
     # Auto-add BGP peers on the WG subnet.
     melinoe.bgpPeers = lib.mkAfter extraBgpPeers;
