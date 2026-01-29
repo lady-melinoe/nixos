a95a594ad8ce2935d46b0152718737f2b39eeab6
Melinoe <admin@melinoe.net>
2026-01-30 07:22:24 +0800

    add benzaiten to wireguard mesh

--- common/wg-keys.nix
+++ common/wg-keys.nix
@@ -3,5 +3,5 @@
   "4" = "0vW0eCXm1xwZohBU50pirElC1K6niM+0ziXToi3O3l4=";
   "5" = "DkBYXIhmAad37oi2SFj52w9+Yr0B5zYs04JOR/YEdmo=";
   "6" = "HKpprvVK3XbEQjw11ecRiyBAPfY+KBZD5hH0OPRNuCc=";
-  "7" = "bsCSoVcsPICc6dBuoVGJVUyxZbaT9UQkAWyv1JljEHA=";
+  "7" = "41zfU8FrcEMtD4MHr2Aq8kj/1oujLpOExeCJe6lyUEE=";
 }
