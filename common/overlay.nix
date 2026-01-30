73261e83abccfb80804fea9346988d03618b9a2d
Melinoe <admin@melinoe.net>
2026-01-30 16:33:40 +0800

    rework overlay module

--- common/overlay.nix
+++ common/overlay.nix
@@ -5,6 +5,71 @@ let
   incusStorageSource = config.melinoe.incusDefaultStorageSource;
   incusRootSize = config.melinoe.incusRootSize;
   incusPreseedEnabled = config.melinoe.enableIncusPreseed;
+  routeListServer = pkgs.writeTextFile {
+    name = "melinoe-route-list.py";
+    destination = "/bin/melinoe-route-list";
+    executable = true;
+    text = ''
+      #!/usr/bin/env python3
+      import http.server
+      import os
+      import subprocess
+      import sys
+
+
+      def gather_routes():
+        cmd = ["sh", "-c", "ip route | grep -F 'dev vm-' | cut -d' ' -f1"]
+        res = subprocess.run(cmd, capture_output=True, text=True)
+        routes = []
+        for line in res.stdout.splitlines():
+          line = line.strip()
+          if not line:
+            continue
+          if "/" not in line:
+            line = f"{line}/32"
+          routes.append(line)
+        return routes
+
+
+      class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, directory=None, **kwargs):
+          super().__init__(*args, directory=directory, **kwargs)
+
+        def do_GET(self):
+          path = self.path.split("?", 1)[0]
+          if path == "/list":
+            routes = gather_routes()
+            body = "\n".join(routes) + "\n"
+            data = body.encode()
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.send_header("Content-Length", str(len(data)))
+            self.end_headers()
+            self.wfile.write(data)
+            return
+          return super().do_GET()
+
+        def log_message(self, format, *args):
+          return
+
+
+      def main():
+        if len(sys.argv) != 4:
+          print("Usage: melinoe-route-list <bind_host> <port> <docroot>")
+          sys.exit(1)
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+        docroot = sys.argv[3]
+        os.chdir(docroot)
+        handler = lambda *args, **kwargs: Handler(*args, directory=docroot, **kwargs)
+        with http.server.ThreadingHTTPServer((host, port), handler) as httpd:
+          httpd.serve_forever()
+
+
+      if __name__ == "__main__":
+        main()
+    '';
+  };
   configureIface = pkgs.writeShellScriptBin "configure-iface" ''
     #!/usr/bin/env bash
     IFACE="$1"
@@ -91,10 +156,23 @@ in {
     services.glances.enable = true;
     services.glances.port = 61208;
     services.glances.extraArgs = [ "--webserver" "-B" "198.18.0.${toString nodeID}" ];
-    services.static-web-server = {
-      enable = true;
-      listen = "198.18.0.${toString nodeID}:60198";
-      root = "/etc/melinoe/residents/";
+
+    systemd.services.melinoe-route-list = {
+      description = "Melinoe resident list server";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.python3}/bin/python ${routeListServer}/bin/melinoe-route-list 198.18.0.${toString nodeID} 60198 /etc/melinoe/residents";
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+      path = [
+        pkgs.iproute2
+        pkgs.coreutils
+        pkgs.gnugrep
+        pkgs.gawk
+      ];
     };
   };
 }
