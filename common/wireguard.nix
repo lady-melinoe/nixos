c2ea3262f25f25eca731fbcdb422b3ff32c54ef7
Melinoe <admin@melinoe.net>
2026-01-30 08:09:11 +0800

    add wireguard setting

--- common/wireguard.nix
+++ common/wireguard.nix
@@ -28,6 +28,7 @@ let
         listenPort = basePort + peer.id;
         privateKeyFile = "/etc/melinoe/wg.privatekey";
         peers = [ (peerToCfg peer) ];
+        table = "off";
         postSetup = ''
           ip address replace ${localWgAddr}/32 peer ${wgPrefix}.${toString peer.id}/32 dev ${ifName}
         '';
