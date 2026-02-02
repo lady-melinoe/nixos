c5d0eabb0e995a44e7c85fa0fdbbaed2cddb25b0
Melinoe <admin@melinoe.net>
2026-02-03 05:17:42 +0800

    add hecate wireguard key

--- common/wg-keys.nix
+++ common/wg-keys.nix
@@ -1,6 +1,6 @@
 {
   "2" = "L0Gkf5z2s6N6juTE95hty4ssrBUQvUT9PCd9PpAVXVg=";
-  "3" = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  "3" = "h8mTzQ/tjBhiitTKPh/R8oDksAKeuFY/I9Kq6/JqMm4=";
   "4" = "0vW0eCXm1xwZohBU50pirElC1K6niM+0ziXToi3O3l4=";
   "5" = "DkBYXIhmAad37oi2SFj52w9+Yr0B5zYs04JOR/YEdmo=";
   "6" = "J1BWw4e4/jwXqYoajGiQZwHqsQxBM+Y079gy46PYzGs=";
