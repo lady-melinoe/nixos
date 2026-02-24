ecbb12c6be021540ff1ea40f20923a5ab93a0ceb
Melinoe <admin@melinoe.net>
2026-02-25 04:35:49 +0800

    fix broken firewall config

--- common/firewall.nix
+++ common/firewall.nix
@@ -49,8 +49,8 @@ ${lib.optionalString (config.melinoe.wgPorts != [ ]) ''
     table inet raw {
       chain prerouting {
         type filter hook prerouting priority raw; policy accept;
-        iifname $inet_ifs ip saddr { 198.18.0.0/15, 198.51.100.0/24 } drop
-        iifname $inet_ifs ip daddr { 198.18.0.0/15, 198.51.100.0/24 } drop
+        iifname $inet_ifs ip saddr { 198.18.0.0/16, 198.51.100.0/24 } drop
+        iifname $inet_ifs ip daddr { 198.18.0.0/16, 198.51.100.0/24 } drop
         iifname $vm_ifs ip saddr 198.18.0.0/24 drop
         iifname $vm_ifs ip saddr != 198.18.0.0/16 drop 
       }
