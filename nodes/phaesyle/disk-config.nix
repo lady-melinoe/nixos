8243c3098028aa98c14995bc0f20f3c63e07f142
Melinoe <admin@melinoe.net>
2026-02-26 16:59:14 +0800

    update phaesyle disk config

--- nodes/phaesyle/disk-config.nix
+++ nodes/phaesyle/disk-config.nix
@@ -7,6 +7,11 @@
         content = {
           type = "gpt";
           partitions = {
+            MBR = {
+              type = "EF02"; # for grub MBR
+              size = "1M";
+              priority = 1; # Needs to be first partition
+            };
             EFI = {
               name = "EFI";
               type = "EF00";
