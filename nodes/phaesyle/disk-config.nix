82de5779799326757d7292599a967fb4ef739d1a
Melinoe <admin@melinoe.net>
2026-02-27 15:02:10 +0800

    revert phaesyle back to EFI boot for another attempt

--- nodes/phaesyle/disk-config.nix
+++ nodes/phaesyle/disk-config.nix
@@ -6,22 +6,7 @@
         device = "/dev/sda";
         content = {
           type = "gpt";
-          efiGptPartitionFirst = false;
           partitions = {
-            TOW-BOOT-FI = {
-              priority = 1;
-              type = "EF00";
-              size = "32M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = null;
-              };
-              hybrid = {
-                mbrPartitionType = "0x0c";
-                mbrBootableFlag = false;
-              };
-            };
             EFI = {
               name = "EFI";
               type = "EF00";
@@ -66,7 +51,6 @@
                   };
                   "/@array" = {
                     mountpoint = "/array";
-                    mountOptions = [ "noatime" ];
                   };
                 };
               };
--- nodes/phaesyle/node.nix
+++ nodes/phaesyle/node.nix
@@ -19,6 +19,7 @@
     efiSupport = true;
     configurationLimit = 20;
     efiInstallAsRemovable = true;
+    device = nodev;
   };
 
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
