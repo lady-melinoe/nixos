255a977c403f0dc04204efe2159fdf25f973babc
Melinoe <admin@melinoe.net>
2026-02-28 10:22:11 +0800

    add QUIC support to firewall

--- common/firewall.nix
+++ common/firewall.nix
@@ -60,6 +60,7 @@ ${lib.optionalString (config.melinoe.wgPorts != [ ]) ''
       chain prerouting {
         type nat hook prerouting priority dstnat;
         iifname $inet_ifs tcp dport { 80, 443 } dnat to 198.18.1.1
+        iifname $inet_ifs udp dport { 80, 443 } dnat to 198.18.1.1
         iifname $inet_ifs tcp dport { 993, 25, 465 } dnat to 198.18.1.6
         iifname $inet_ifs tcp dport { 25565 } dnat to 198.18.1.8
         iifname $inet_ifs tcp dport { 57843 } dnat to 198.18.1.11
