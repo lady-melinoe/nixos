d82041d77f253efb918f0b34201835a6672299ad
Melinoe <admin@melinoe.net>
2026-03-09 18:13:32 +0800

    fix arke node config

--- nodes/arke/node.nix
+++ nodes/arke/node.nix
@@ -48,7 +48,7 @@
       gateway = "130.95.13.129";
     }
     {
-      ip = "198.19.0.${toString config.melinoe.nodeId}/24";
+      ip = "198.19.0.${toString config.melinoe.nodeId}/32";
       iface = [ "ens19" ];
       subnet = "198.19.0.0/24";
       gateway = null;
