109ad791d1def2997fb123653343cc3309ab442f
Melinoe <admin@melinoe.net>
2026-03-20 14:15:46 +0800

    add SSH host certs

--- common/settings.nix
+++ common/settings.nix
@@ -1,10 +1,25 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+
+let
+  nodeCertDir = ../nodes/${config.networking.hostName};
+in
 
 {
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
   services.openssh.enable = true;
   services.openssh.ports = [ 22 ];
+  services.openssh.extraConfig = ''
+    HostCertificate /etc/ssh/ssh_host_ed25519_key-cert.pub
+  '';
+  environment.etc."ssh/ssh_host_ed25519_key.pub" = {
+    source = "${nodeCertDir}/ssh_host_ed25519_key.pub";
+    mode = "0644";
+  };
+  environment.etc."ssh/ssh_host_ed25519_key-cert.pub" = {
+    source = "${nodeCertDir}/ssh_host_ed25519_key-cert.pub";
+    mode = "0644";
+  };
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.require-sigs = false;
   system.stateVersion = "25.05";
