3ca5b853896f9b6fea5f275d18cd10c38b223570
Melinoe <admin@melinoe.net>
2026-03-28 03:50:36 +0800

    remove stale networking config line

--- common/networking.nix
+++ common/networking.nix
@@ -58,7 +58,6 @@ let
         privateKeyFile = "/etc/melinoe/wg.privatekey";
         peers = [ (peerToCfg peer) ];
         postUp = ''
-          ip address replace ${localWgAddr}/32 peer ${wgPrefix}.${toString peer.id}/32 dev ${ifName}
           ${runtimeShell} -c '${ipBin} route del 198.19.3.0/24 dev ${ifName} || true'
           ${runtimeShell} -c '${ipBin} route del 198.51.100.0/24 dev ${ifName} || true'
         '';
