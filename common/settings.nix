8af2f7e1b575266b3524a9020883d8ba0da22686
Melinoe <admin@melinoe.net>
2026-03-28 17:22:13 +0800

    add borg package

--- common/settings.nix
+++ common/settings.nix
@@ -28,7 +28,7 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.require-sigs = false;
   system.stateVersion = "25.05";
-  environment.systemPackages = [ pkgs.git pkgs.tcpdump pkgs.nftables pkgs.jq pkgs.screen pkgs.btop pkgs.iperf3 pkgs.iptables pkgs.python3 ];
+  environment.systemPackages = [ pkgs.git pkgs.tcpdump pkgs.nftables pkgs.jq pkgs.screen pkgs.btop pkgs.iperf3 pkgs.iptables pkgs.python3 pkgs.borgbackup ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernel.sysctl = {
     "net.ipv6.conf.all.autoconf" = false;
