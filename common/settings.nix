2ac890c65c739fb3e46b93d0153b853474f0feb1
Melinoe <admin@melinoe.net>
2026-03-29 23:41:03 +0800

    fix settings error

--- common/settings.nix
+++ common/settings.nix
@@ -39,7 +39,7 @@
     pkgs.iperf3
     pkgs.iptables
     pkgs.python3
-    inputs.self.packages.${pkgs.system}.borg-beta
+    inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.borg-beta
   ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernel.sysctl = {
