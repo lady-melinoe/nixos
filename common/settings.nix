e91c6b1c682631680783c5f5b20f9f9f6d3ed322
Melinoe <admin@melinoe.net>
2026-06-24 15:41:05 +0800

    rework networking config

--- common/settings.nix
+++ common/settings.nix
@@ -70,6 +70,7 @@
     pkgs.compsize
     pkgs.aria2
     pkgs.nfs-utils
+    pkgs.sshfs
     inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.borg-beta
   ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
