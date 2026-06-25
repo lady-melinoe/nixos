33e93bf781a7abc48454e3ef34aae1d6a56f8732
Melinoe <admin@melinoe.net>
2026-06-25 19:03:56 +0800

    rework CI patterns, add meldeploy module

--- flake.nix
+++ flake.nix
@@ -43,33 +43,48 @@
         inherit pkgs lib nixosConfigurations;
       };
 
+      meldeploy = import ./meldeploy.nix {
+        inherit pkgs lib nixosConfigurations;
+      };
+
     in
     {
       formatter.${system} = pkgs.nixfmt-tree;
 
-      packages.${system} = {
-        borg-beta = pkgs.stdenvNoCC.mkDerivation {
-          pname = "borg-beta";
-          version = "2.0.0b21";
+      packages.${system} =
+        {
+          borg-beta = pkgs.stdenvNoCC.mkDerivation {
+            pname = "borg-beta";
+            version = "2.0.0b21";
 
-          src = pkgs.fetchurl {
-            url = "https://github.com/borgbackup/borg/releases/download/2.0.0b21/borg-linux-glibc235-x86_64-gh";
-            hash = "sha256-HN5TudJIwaYA32+TjuoyB/JxQb4OjXqAVC+o+QHyIcU=";
+            src = pkgs.fetchurl {
+              url = "https://github.com/borgbackup/borg/releases/download/2.0.0b21/borg-linux-glibc235-x86_64-gh";
+              hash = "sha256-HN5TudJIwaYA32+TjuoyB/JxQb4OjXqAVC+o+QHyIcU=";
+            };
+
+            dontUnpack = true;
+            dontBuild = true;
+
+            installPhase = ''
+              install -Dm755 "$src" "$out/bin/borg"
+            '';
           };
+        }
+        // melssh
+        // meldeploy;
 
-          dontUnpack = true;
-          dontBuild = true;
-
-          installPhase = ''
-            install -Dm755 "$src" "$out/bin/borg"
-          '';
+      apps.${system} = {
+        mel-ssh-host-ca = {
+          type = "app";
+          program = "${melssh.mel-ssh-host-ca}/bin/mel-ssh-host-ca";
+          meta.description = "Melinoe SSH host CA management tool";
         };
-      };
 
-      apps.${system}.mel-ssh-host-ca = {
-        type = "app";
-        program = "${melssh.mel-ssh-host-ca}/bin/mel-ssh-host-ca";
-        meta.description = "Melinoe SSH host CA management tool";
+        mel-deploy = {
+          type = "app";
+          program = "${meldeploy.mel-deploy}/bin/mel-deploy";
+          meta.description = "Melinoe fleet deployment helper (parses [DEPLOY] commit tags)";
+        };
       };
 
       inherit nixosConfigurations;
