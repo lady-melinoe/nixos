3d814d3b9edc05e86ceb3d5dbc7b4de9fa142c72
Melinoe <admin@melinoe.net>
2026-07-03 09:13:01 +0800

    re-add auditd rules

--- common/settings.nix
+++ common/settings.nix
@@ -36,6 +36,14 @@
   security.audit.enable = true;
   security.auditd.enable = true;
   services.qemuGuest.enable = true;
+  security.auditd.settings = {
+    log_format = "ENRICHED";
+  };
+  security.audit.rules = [
+    "-a always,exit -F arch=b64 -S execve -F euid=0 -F key=root_cmd"
+    "-a always,exit -F arch=b32 -S execve -F euid=0 -F key=root_cmd"
+  ];
+
   systemd.services.qemu-guest-agent.unitConfig.ConditionVirtualization = "vm";
   services.openssh.ports = [ 22 ];
   services.openssh.extraConfig = ''
