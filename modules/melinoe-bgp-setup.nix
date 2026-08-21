1c720684eca3d281564ee2bb870bcb520deb9bdd
Melinoe <admin@melinoe.net>
2026-08-22 01:21:01 +0800

    extend BFD testing in bgp-setup module

--- modules/melinoe-bgp-setup.nix
+++ modules/melinoe-bgp-setup.nix
@@ -10,51 +10,90 @@ let
   netCfg = config.melinoe.node.networking;
   addr = config.melinoe.cluster.networking;
   nodeID = cfg.node.id;
+
   bgpAsBase = 64512; # start of private ASN range (RFC 6996)
   bgpAs = bgpAsBase + nodeID;
+
   localWgAddr = melinoeNodeWgIP nodeID;
   localLoopbackAddr = melinoeNodeLoopbackIP nodeID;
+
   peers = map (p: {
     id = p.id;
     addr = melinoeNodeWgIP p.id;
   }) netCfg.peers;
+
   mkNeighborStanza = peer: ''
     neighbor ${peer.addr} remote-as ${toString (bgpAsBase + peer.id)}
     neighbor ${peer.addr} update-source ${localWgAddr}
     neighbor ${peer.addr} timers 1 3
   '';
+
   mkNeighborAfi = peer: ''
     neighbor ${peer.addr} activate
     neighbor ${peer.addr} route-map NODE-IN in
     neighbor ${peer.addr} route-map NODE-OUT out
   '';
+
+  # Temporary BFD test: only enable bfdd and configure a BFD
+  # session on nodes 6 and 7.
+  bfdEnabled = nodeID == 6 || nodeID == 7;
+
+  bfdPeer =
+    if nodeID == 6 then {
+      addr = melinoeNodeWgIP 7;
+      localAddr = localWgAddr;
+      interface = "wg-7";
+    } else if nodeID == 7 then {
+      addr = melinoeNodeWgIP 6;
+      localAddr = localWgAddr;
+      interface = "wg-6";
+    } else
+      null;
+
+  bfdConfig = lib.optionalString bfdEnabled ''
+    bfd
+      peer ${bfdPeer.addr} local-address ${bfdPeer.localAddr} interface ${bfdPeer.interface}
+        transmit-interval 300
+        receive-interval 300
+        detect-multiplier 3
+      exit
+    exit
+  '';
 in
 {
   config = {
-    melinoe.node.networking.specialWgAccess.tcp = lib.mkIf netCfg.enabled [ 179 ];
+    melinoe.node.networking.specialWgAccess.tcp =
+      lib.mkIf netCfg.enabled [ 179 ];
 
     services.frr = lib.mkIf netCfg.enabled {
       bgpd.enable = true;
+      bfdd.enable = bfdEnabled;
+
       config =
         let
           neighborLines = lib.concatMapStrings mkNeighborStanza peers;
           neighborAfiLines = lib.concatMapStrings mkNeighborAfi peers;
         in
         ''
-                  ip prefix-list NODE-LOOPS permit ${addr.bgpCidr} le 32
-                  route-map NODE-IN permit 10
-                   match ip address prefix-list NODE-LOOPS
-                  route-map NODE-OUT permit 10
-                   match ip address prefix-list NODE-LOOPS
-                  router bgp ${toString bgpAs}
-                    bgp router-id ${localLoopbackAddr}
-                    bgp fast-convergence
+          ip prefix-list NODE-LOOPS permit ${addr.bgpCidr} le 32
+
+          route-map NODE-IN permit 10
+           match ip address prefix-list NODE-LOOPS
+
+          route-map NODE-OUT permit 10
+           match ip address prefix-list NODE-LOOPS
+
+          ${bfdConfig}
+
+          router bgp ${toString bgpAs}
+            bgp router-id ${localLoopbackAddr}
+            bgp fast-convergence
           ${neighborLines}
-                    address-family ipv4 unicast
-                      network ${localLoopbackAddr}/32
+            address-family ipv4 unicast
+              network ${localLoopbackAddr}/32
           ${neighborAfiLines}
-                    exit-address-family
-                  !
+            exit-address-family
+          !
         '';
     };
   };
