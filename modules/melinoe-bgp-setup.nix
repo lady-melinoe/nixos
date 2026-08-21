2092a77da389a00730b1d7083b6e12de43032cd5
Melinoe <admin@melinoe.net>
2026-08-22 01:51:49 +0800

    rework bgp-setup module

--- modules/melinoe-bgp-setup.nix
+++ modules/melinoe-bgp-setup.nix
@@ -9,6 +9,7 @@ let
   cfg = config.melinoe;
   netCfg = config.melinoe.node.networking;
   addr = config.melinoe.cluster.networking;
+
   nodeID = cfg.node.id;
 
   bgpAsBase = 64512; # start of private ASN range (RFC 6996)
@@ -22,49 +23,43 @@ let
     addr = melinoeNodeWgIP p.id;
   }) netCfg.peers;
 
-  # Temporary BFD test: only nodes 6 and 7 participate.
-  bfdEnabled = nodeID == 6 || nodeID == 7;
+  # Temporary experiment:
+  # nodes 6 and 7 use interface-based BGP over their direct WG link.
+  interfaceBGP = nodeID == 6 || nodeID == 7;
 
-  bfdPeer =
-    if nodeID == 6 then {
-      addr = melinoeNodeWgIP 7;
-      localAddr = localWgAddr;
-    } else if nodeID == 7 then {
-      addr = melinoeNodeWgIP 6;
-      localAddr = localWgAddr;
-    } else
-      null;
+  isTargetPeer = peer:
+    interfaceBGP
+    && (
+      (nodeID == 6 && peer.id == 7)
+      || (nodeID == 7 && peer.id == 6)
+    );
 
-  # Multihop BFD deliberately does not specify an interface.
-  # This lets us test whether FRR follows normal routing to the peer.
-  bfdConfig = lib.optionalString bfdEnabled ''
-    bfd
-      peer ${bfdPeer.addr} multihop local-address ${bfdPeer.localAddr}
-        transmit-interval 300
-        receive-interval 300
-        detect-multiplier 3
-      exit
-    exit
-  '';
+  mkNeighborStanza = peer:
+    if isTargetPeer peer then
+      ''
+        neighbor wg-${toString peer.id} interface remote-as ${toString (bgpAsBase + peer.id)}
+        neighbor wg-${toString peer.id} bfd
+      ''
+    else
+      ''
+        neighbor ${peer.addr} remote-as ${toString (bgpAsBase + peer.id)}
+        neighbor ${peer.addr} update-source ${localWgAddr}
+        neighbor ${peer.addr} timers 1 3
+      '';
 
-  mkNeighborStanza = peer: ''
-    neighbor ${peer.addr} remote-as ${toString (bgpAsBase + peer.id)}
-    neighbor ${peer.addr} update-source ${localWgAddr}
-    neighbor ${peer.addr} timers 1 3
-    ${lib.optionalString (
-      bfdEnabled
-      && (
-        (nodeID == 6 && peer.id == 7)
-        || (nodeID == 7 && peer.id == 6)
-      )
-    ) "neighbor ${peer.addr} bfd"}
-  '';
-
-  mkNeighborAfi = peer: ''
-    neighbor ${peer.addr} activate
-    neighbor ${peer.addr} route-map NODE-IN in
-    neighbor ${peer.addr} route-map NODE-OUT out
-  '';
+  mkNeighborAfi = peer:
+    if isTargetPeer peer then
+      ''
+        neighbor wg-${toString peer.id} activate
+        neighbor wg-${toString peer.id} route-map NODE-IN in
+        neighbor wg-${toString peer.id} route-map NODE-OUT out
+      ''
+    else
+      ''
+        neighbor ${peer.addr} activate
+        neighbor ${peer.addr} route-map NODE-IN in
+        neighbor ${peer.addr} route-map NODE-OUT out
+      '';
 in
 {
   config = {
@@ -73,7 +68,7 @@ in
 
     services.frr = lib.mkIf netCfg.enabled {
       bgpd.enable = true;
-      bfdd.enable = bfdEnabled;
+      bfdd.enable = true;
 
       config =
         let
@@ -89,8 +84,6 @@ in
           route-map NODE-OUT permit 10
            match ip address prefix-list NODE-LOOPS
 
-          ${bfdConfig}
-
           router bgp ${toString bgpAs}
             bgp router-id ${localLoopbackAddr}
             bgp fast-convergence
@@ -101,6 +94,15 @@ in
             exit-address-family
           !
         '';
+    };
+
+    # FRR currently has an upstream reload problem for us.
+    # Make NixOS stop and start the service when its definition changes,
+    # rather than attempting a systemd reload.
+    systemd.services.frr = lib.mkIf netCfg.enabled {
+      reloadIfChanged = false;
+      restartIfChanged = true;
+      stopIfChanged = true;
     };
   };
 }
