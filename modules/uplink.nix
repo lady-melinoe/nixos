e87b9c2b0055c43b59dcd64ca275ebc0884b52a4
Melinoe <admin@melinoe.net>
2026-08-28 05:46:02 +0800

    Temporary Fix

--- modules/uplink.nix
+++ modules/uplink.nix
@@ -5,6 +5,7 @@
   ...
 }:
 let
+  nid = config.melinoe.node.id;
   netCfg = config.melinoe.node.networking;
   table = toString netCfg.uplinkFwMark;
   uplinkIface =
@@ -90,6 +91,11 @@ in
         ExecStart = pkgs.writeShellScript "melinoe-inet-setup" ''
           ip rule del fwmark ${table} lookup ${table} >/dev/null 2>&1 || true
           ip rule add fwmark ${table} lookup ${table}
+          ip rule del pref 1 from all lookup local >/dev/null 2>&1 || true
+          ip rule add pref 1 from all lookup local
+          ip rule del pref 0 from all lookup local >/dev/null 2>&1 || true
+          ip addr del 198.18.0.${nid}/32 dev lo >/dev/null 2>&1 || true
+          ip addr add 198.18.0.${nid}/32 dev lo
           sysctl -w net.ipv4.conf.default.rp_filter=0
           sysctl -w net.ipv4.conf.all.rp_filter=0
           ${lib.concatStringsSep "\n" (lib.imap0 mkUplinkScript netCfg.uplinks)}
