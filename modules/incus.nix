e1d898c90cc86ca32813f97e2230bd848a81bc9c
Melinoe <admin@melinoe.net>
2026-09-04 00:27:14 +0800

    Update

--- modules/incus.nix
+++ modules/incus.nix
@@ -13,15 +13,14 @@ in
     virtualisation.incus.enable = true;
     virtualisation.incus.package = pkgs.incus;
     virtualisation.incus.softDaemonRestart = true;
-    melinoe.node.networking.openPorts.tcp = [
-      8008 # incus
-      8069 # incus
-    ];
+    melinoe.node.networking.specialHostAccess.tcp = [ 8008 ]; # Incus Cluster
+    melinoe.node.networking.openPorts.tcp = [ 8069 ]; # Incus MGMT
     users.users.melinoe.extraGroups = [ "incus-admin" ];
     programs.bash.shellAliases = {
       icl = "incus cluster list -c nursm";
       ie = "incus exec";
       il = "incus list '-cdevices:uplink.ipv4.address:v4ADDR,nstL,devices:uplink.ipv4.routes:ADDITIONAL ROUTES'";
+      imv = "incus move --refresh --stateless";
     };
   };
 }
