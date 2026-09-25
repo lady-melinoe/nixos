60c793eb45c3d80096a2a3e666d7b45e0b5f4894
Melinoe <admin@melinoe.net>
2026-09-25 21:26:27 +0800

    Enable Hecate

--- nodes/hecate/node.nix
+++ nodes/hecate/node.nix
@@ -34,6 +34,8 @@
     }
   ];
   melinoe.services.melnode.extraRoutes = [ "130.95.13.0/24" ];
+  melinoe.services.melnode.kernelDataplane.enable = true;
+  melinoe.services.melnode.dataplane = "kernel";
   melinoe.node.networking.peers = [
     {
       id = 5;
